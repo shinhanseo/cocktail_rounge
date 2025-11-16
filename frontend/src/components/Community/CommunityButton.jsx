05563013818c4c97012ecf29098012d1864e398c
shinhanseo <imkara1@naver.com>
2025-11-16 13:09:57 +0900

    글쓰기 버튼 수정

--- frontend/src/components/Community/CommunityButton.jsx
+++ frontend/src/components/Community/CommunityButton.jsx
@@ -22,13 +22,9 @@ export default function CommunityButton() {
     <div className="text-right mr-8">
       <button
         onClick={onClick}
-        className="text-white ml-5 p-2 bg-white rounded-full hover:scale-110 hover:cursor-pointer"
+        className="text-white ml-5 p-2 bg-white/95 rounded-full hover:scale-110 hover:cursor-pointer"
       >
-        <img
-          src={postButton}
-          alt="글쓰기 버튼"
-          className="w-6 h-6 object-cover"
-        />
+        <img src={postButton} alt="글쓰기 버튼" className="w-6 h-6" />
       </button>
     </div>
   );
